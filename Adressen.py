5fcbf8b87aa65c63c39ef237062cf50f3437d161
PsychzZ <55191856+PsychzZ@users.noreply.github.com>
2019-12-03 10:48:34 +0100

    Merge branch 'master' of /Users/max/Documents/GIT Scheiße with conflicts.

--- Adressen.py
+++ Adressen.py
@@ -1,30 +1,25 @@
 import argparse
 import sqlite3
+
 # Eingaben
 parser = argparse.ArgumentParser()
-adressen = parser.add_argument_group(title="Adressierungs Befehle")
-aufgaben = parser.add_mutually_exclusive_group()
-ausgaben = parser.add_argument_group(title="Ausgabe Befehle")
-# Adressierungen
-adressen.add_argument("--firstname", help="Vorname",)
-adressen.add_argument("--lastname", help="Nachname",)
-adressen.add_argument("--street", help="Staße",)
-adressen.add_argument("--number", help="Hausnumemr",)
-adressen.add_argument("--postal-code", help="Postleitzahl", type=int)
-adressen.add_argument("--place", help="Ort",)
-adressen.add_argument("--birthday", help="Das Geburtsdatum in YY-MM-DD",)
-adressen.add_argument("--landline", help="Festnetznummer",)
-adressen.add_argument("--mobile", help="Handynummer",)
-adressen.add_argument("--mail", help="E-Mail",)
-# Aufgaben
-aufgaben.add_argument("--update", action="store_true", help="hinzufügen")
-aufgaben.add_argument("--delete", action="store_true", help="etwas löschen")
-aufgaben.add_argument("--search", action="store_true", help="Suchen")
-aufgaben.add_argument("--get", action="store_true", help="?", )
-# Ausgaben
-ausgaben.add_argument("--full", action="store_true", help="Gibt die Datenbank aus")
-ausgaben.add_argument("--names", action="store_true", help="Gibt die Id´s der Personen aus")
-ausgaben.add_argument("--field", action="store_true", help="Gibt ein beszimmten wert aus")
+parser.add_argument("--firstname", help="Vorname", )
+parser.add_argument("--lastname", help="Nachname", )
+parser.add_argument("--street", help="Staße", )
+parser.add_argument("--number", help="Hausnumemr", )
+parser.add_argument("--postal-code", help="Postleitzahl", type=int)
+parser.add_argument("--place", help="Ort", )
+parser.add_argument("--birthday", help="Das Geburtsdatum in YY-MM-DD", )
+parser.add_argument("--landline", help="Festnetznummer", )
+parser.add_argument("--mobile", help="Handynummer", )
+parser.add_argument("--mail", help="E-Mail", )
+# Abfragen
+parser.add_argument("--update", action="store_true", help="hinzufügen")
+parser.add_argument("--delete", action="store_true", help="etwas löschen")
+parser.add_argument("--get", action="store_true", help="?", )
+parser.add_argument("--full", action="store_true", help="Gibt die Datenbank aus")
+parser.add_argument("--names", action="store_true", help="Gibt die Id´s der Personen aus")
+parser.add_argument("--field", action="store_true", help="Gibt ein beszimmten wert aus")
 
 args = parser.parse_args()
 
@@ -50,16 +45,12 @@ class Adressen:
 class Abfragen:
     def __init__(self, args):
         self.args = args
-        self.full = args.full
-        self.names = args.names
-        self.field = args.field
-        self.search = args.search
-
-
         self.update = args.update
         self.delete = args.delete
         self.get = args.get
-
+        self.full = args.full
+        self.names = args.names
+        self.field = args.field
 
 
 a_info = Abfragen(args)
@@ -88,38 +79,38 @@ else:
 print(info.insert_list)
 print(info.insert_list[0], info.insert_list[1], info.insert_list[2], info.insert_list[3])
 
+
 class AddressDatabase:
 
     def __enter__(self):
         return self
 
     def __del__(self):
-        self.__db_connection.close()
+        self.connection.close()
 
     def __init__(self):
         """Initialize db class variables"""
         self.connection = sqlite3.connect("Adressen.db")
-        self.cur = self.connection.cursor()
+        self.cursor = self.connection.cursor()
 
     def __exit__(self):
         self.cursor.close()
 
-
     def execute(self, new_data):
         """add many new data to database in one go"""
         self.create_table()
-        self.cur.execute(""" INSERT INTO Adressen(Id, Firstname,Lastname,Birthday,Street,Number,Postalcode,
-                                Place,Landline,Mobile,Mail) VALUES(?,?,?,?,?,?,?,?,?,?,?,?);""", new_data)
+        self.cursor.execute(""" INSERT INTO Adressen(Id, Firstname,Lastname,Birthday,Street,Number,Postalcode,
+                                Place,Landline,Mobile,Mail) VALUES(?,?,?,?,?,?,?,?,?,?,?);""", new_data)
 
     def create_table(self):
         """create a database table if it does not exist already"""
-        self.cur.execute("""CREATE TABLE IF NOT EXISTS Adressen (Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        create = self.cursor.execute("""CREATE TABLE IF NOT EXISTS Adressen (Id INTEGER PRIMARY KEY AUTOINCREMENT,
                                              Firstname VARCHAR (50),
                                              Lastname VARCHAR(50),
                                              Birthday Varchar(50),
                                              Street VARCHAR (50),
                                              Number VARCHAR(50),
-                                             Postalcode INTEGER,
+                                             Postalcode VARCHAR(50),
                                              Place VARCHAR(50),
                                              Landline VARCHAR (50),
                                              Mobile VARCHAR(50),
